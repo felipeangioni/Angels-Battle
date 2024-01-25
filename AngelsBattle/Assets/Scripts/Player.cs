5d9d2114bda4e368fa8b77b0dfd791d0d3945f5a
Felipe Angioni <131915025+felipeangioni@users.noreply.github.com>
2024-01-25 15:44:31 -0300

    Losing life

--- AngelsBattle/Assets/Scripts/Player.cs
+++ AngelsBattle/Assets/Scripts/Player.cs
@@ -2,28 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SafeZone : MonoBehaviour
+public class Player : MonoBehaviour
 {
-    public static float RelativeCenterDistanceSZ;
+    public float RelativeCenterDistanceSZ;
     public Vector3 EdgePosSZ;
     public Vector3 CenterPosSZ;
+    public float Life;
     void Start()
     {
-        
+        Life = 100f;
     }
 
-    
     void Update()
     {
         EdgePosSZ = transform.position;
         CenterPosSZ = GameObject.FindWithTag("SafeZoneCenter").transform.position;
 
-        if(EdgePosSZ.x < 0)
+        if (EdgePosSZ.x < 0)
         {
             EdgePosSZ.x = -EdgePosSZ.x;
         }
 
-        if(EdgePosSZ.z < 0)
+        if (EdgePosSZ.z < 0)
         {
             EdgePosSZ.z = -EdgePosSZ.z;
         }
@@ -39,10 +39,16 @@ public class SafeZone : MonoBehaviour
         }
 
         RelativeCenterDistanceSZ = Vector3.Distance(EdgePosSZ, CenterPosSZ);
-    }
+
+        if(RelativeCenterDistanceSZ > SafeZone.RelativeCenterDistanceSZ)
+        {
+            Life -= 1 * Time.deltaTime;
+        }
+    } 
 
     void OnGUI()
     {
-        GUI.TextField(new Rect(10f, 10f, 130f, 20f), "Distance:" + RelativeCenterDistanceSZ);
+        GUI.TextField(new Rect(10f, 30f, 130f, 20f), "Distance:" + RelativeCenterDistanceSZ);
+        GUI.TextField(new Rect(200f, 700f, 130f, 20f), "Life:" + RelativeCenterDistanceSZ);
     }
 }
