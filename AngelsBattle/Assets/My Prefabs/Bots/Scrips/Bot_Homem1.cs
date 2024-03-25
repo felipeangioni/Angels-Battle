52a0a564787749cfeef7c02e460c1747f72cbad8
Felipe Angioni <131915025+felipeangioni@users.noreply.github.com>
2024-03-25 17:35:20 -0300

    Navmesh
    Beggining to implement navmesh on bot

--- AngelsBattle/Assets/My Prefabs/Bots/Scrips/Bot_Homem1.cs
+++ AngelsBattle/Assets/My Prefabs/Bots/Scrips/Bot_Homem1.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
 
 public class Bot_Homem1 : MonoBehaviour
 {
